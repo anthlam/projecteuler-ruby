20c0e64094468911b6cd2cfa11c06240fb146ce8
Anthony Lamorte <lamorte@gmail.com>
2016-11-17 20:53:06 -0500

    Problem 1, second pass, 1 line

--- 1-multiples_of_3_and_5.rb
+++ 1-multiples_of_3_and_5.rb
@@ -1,17 +1,1 @@
-def multiple_of_3?(num)
-  num % 3 == 0
-end
-
-def multiple_of_5?(num)
-  num % 5 == 0
-end
-
-def multiples_of_3_and_5(num)
-  (1...num).select { |n| multiple_of_3?(n) || multiple_of_5?(n) }
-end
-
-def sum_of_multiples_of_3_and_5(num)
-  multiples_of_3_and_5(num).reduce(:+)
-end
-
-puts sum_of_multiples_of_3_and_5(1000)
+puts (1...1000).select { |i| i % 3 == 0 || i % 5 == 0 }.reduce(:+)
