d98a7034a1298ef02d542795aac7cfbdde792109
Anthony Lamorte <lamorte@gmail.com>
2016-11-20 19:59:27 -0500

    Add tests for problem 2

---
+++ tests/2-even_fibonaccis_test.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+require_relative '../2-even_fibonaccis.rb'
+
+class EvenFibonacciTests < Test::Unit::TestCase
+
+  def test_fibonacci_numbers
+    expected_fibs = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    actual_fibs = fibonacci_numbers(100)
+
+    assert_equal expected_fibs, actual_fibs
+  end
+
+  def test_even_fibonacci_numbers
+    expected_even_fibs = [2, 8, 34]
+    actual_even_fibs = even_fibonacci_numbers(100)
+
+    assert_equal expected_even_fibs, actual_even_fibs
+  end
+
+  def test_sum_even_fibonacci_numbers
+    expected_sum = 44
+    actual_sum = sum_even_fibonacci_numbers(100)
+
+    assert_equal expected_sum, actual_sum
+  end
+end
