34ae950665a5d8391662a459d22568fe3a0954f4
Anthony Lamorte <lamorte@gmail.com>
2016-11-20 20:03:46 -0500

    Problem 2, third pass, reducing amount of code

--- 2-even_fibonaccis.rb
+++ 2-even_fibonaccis.rb
@@ -1,20 +1,16 @@
-def fibonaccis(num)
+def even_fibonaccis(num)
   a = b = 1
   next_fib = 0
   fibs = []
 
   while b < num do
-    fibs << b
+    fibs << b if b.even?
     next_fib = a + b
     a = b
     b = next_fib
   end
 
   fibs
-end
-
-def even_fibonaccis(num)
-  fibonaccis(num).select { |n| n % 2 == 0 }
 end
 
 def sum_even_fibonaccis(num)
--- tests/2-even_fibonaccis_test.rb
+++ tests/2-even_fibonaccis_test.rb
@@ -3,13 +3,6 @@ require_relative '../2-even_fibonaccis.rb'
 
 class EvenFibonacciTests < Test::Unit::TestCase
 
-  def test_fibonacci_numbers
-    expected_fibs = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-    actual_fibs = fibonaccis(100)
-
-    assert_equal expected_fibs, actual_fibs
-  end
-
   def test_even_fibonacci_numbers
     expected_even_fibs = [2, 8, 34]
     actual_even_fibs = even_fibonaccis(100)
